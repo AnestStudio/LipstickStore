f129f92f6482a19fa81c919e264468ec038d6cac
Đinh Tuấn Anh <anest.studio@gmail.com>
2024-06-19 07:23:58 +0700

    c

--- src/main/java/org/anest/mystore/service/impl/ReportServiceImpl.java
+++ src/main/java/org/anest/mystore/service/impl/ReportServiceImpl.java
@@ -2,7 +2,7 @@ package org.anest.mystore.service.impl;
 
 import org.anest.mystore.entity.Product;
 import org.anest.mystore.service.ReportService;
-import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -18,40 +18,46 @@ public class ReportServiceImpl implements ReportService {
     @Override
     public void outputExcelListProduct(ByteArrayOutputStream stream, List<Product> products) throws IOException {
 
-        XSSFWorkbook workbook = new XSSFWorkbook();
-        XSSFSheet sheet = workbook.createSheet("products");
+        try (XSSFWorkbook workbook = new XSSFWorkbook();) {
+            XSSFSheet sheet = workbook.createSheet("products");
 
-        int rowCount = 0;
-        for (Product product : products) {
-            XSSFRow row = sheet.createRow(rowCount++);
-
-            int cellCount = 0;
-            for(int i = 0; i < Product.class.getDeclaredFields().length; i++) {
-                Cell cell = row.createCell(cellCount++);
-                if (cellCount == 1)
-                    cell.setCellValue(product.getId());
-                if (cellCount == 2)
-                    cell.setCellValue(product.getProductName());
-                if (cellCount == 3)
-                    cell.setCellValue(product.getProductColor());
-                if (cellCount == 4)
-                    cell.setCellValue(product.getProductImage());
-                if (cellCount == 5)
-                    cell.setCellValue(product.getProductPrice());
-                if (cellCount == 6)
-                    cell.setCellValue(product.getProductQuantity());
-                if (cellCount == 7)
-                    cell.setCellValue(product.getProductShortDescription());
-                if (cellCount == 8)
-                    cell.setCellValue(product.getProductDescription());
-                if (cellCount == 9)
-                    cell.setCellValue(product.getCategory().getCategoryName());
-                if (cellCount == 10)
-                    cell.setCellValue(product.getBrand().getBrandName());
-                if (cellCount == 11)
-                    cell.setCellValue(product.getProductImageList().size());
+            int rowCount = 0;
+            XSSFRow headerRow = sheet.createRow(rowCount++);
+            String[] headers = {
+                    "ID",
+                    "PRODUCT NAME",
+                    "PRODUCT COLOR",
+                    "PRODUCT IMAGE",
+                    "PRODUCT PRICE",
+                    "PRODUCT QUANTITY",
+                    "PRODUCT SHORT DESCRIPTION",
+                    "PRODUCT DESCRIPTION",
+                    "CATEGORY",
+                    "BRAND",
+                    "TOTAL IMAGE"
+            };
+            for (int i = 0; i < headers.length; i++) {
+                XSSFCell cell = headerRow.createCell(i);
+                cell.setCellValue(headers[i]);
             }
+
+            for (Product product : products) {
+                XSSFRow row = sheet.createRow(rowCount++);
+
+                int cellCount = 0;
+                row.createCell(cellCount++).setCellValue(product.getId());
+                row.createCell(cellCount++).setCellValue(product.getProductName());
+                row.createCell(cellCount++).setCellValue(product.getProductColor());
+                row.createCell(cellCount++).setCellValue(product.getProductImage());
+                row.createCell(cellCount++).setCellValue(product.getProductPrice());
+                row.createCell(cellCount++).setCellValue(product.getProductQuantity());
+                row.createCell(cellCount++).setCellValue(product.getProductShortDescription());
+                row.createCell(cellCount++).setCellValue(product.getProductDescription());
+                row.createCell(cellCount++).setCellValue(product.getCategory().getCategoryName());
+                row.createCell(cellCount++).setCellValue(product.getBrand().getBrandName());
+                row.createCell(cellCount).setCellValue(product.getProductImageList().size());
+            }
+            workbook.write(stream);
         }
-        workbook.write(stream);
     }
 }
